2b4eae5782ebdbea570df2f735235bbe51c8b3e6
AlexS28 <45216484+AlexS28@users.noreply.github.com>
2026-03-16 21:52:57 +0000

    Deploying to gh-pages from @ AlexS28/AlexS28.github.io@174c0b9aa83141a0c4074901028e7192f3be3c08 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,7 +461,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Schperberg_CV_2026.pdf", "_blank");
         },
       },{
         id: 'social-email',
